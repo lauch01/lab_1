f55e78361a55c08c9daf65b7aa93e9b9c6fc2e2a
GitUser2004 <dylan.gutierrez.h@gmail.com>
2025-02-17 21:27:51 +0000

    nose

---
+++ exer3.py
@@ -0,0 +1,5 @@
+name = input("whats your name")
+age = input("what your age")
+prof = input("whats your proffesion")
+
+print("hi your name is ", name, ",your age is ", age, " and your proffesion is ",prof)
